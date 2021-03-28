d83cad499644175d762b2dae66c97fc70fb246f9
Eduard Metzler <eduard.metzler@mail.ru>
2021-03-29 01:52:47 +0200

    ddddd

--- config/default.js
+++ config/default.js
@@ -1,7 +1,16 @@
 
-if(process.env.NODE_ENV ==='production'){
-  module.exports=require('./prod')
+// if(process.env.NODE_ENV ==='production'){
+//   module.exports=require('./prod')
 
-}else{
-  module.exports=require('./dev')
+// }else{
+//   module.exports=require('./dev')
+// }
+
+module.exports={
+    
+  port: 5000,
+  jwtSecter: "0000",
+  mongoUri: "mongodb+srv://eduard:eduard@cluster0-urhqd.azure.mongodb.net/app?retryWrites=true&w=majority"
+
+
 }
