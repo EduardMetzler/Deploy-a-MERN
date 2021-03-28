f52767b23e277f9b2df9d2e30f7d6ae06ece6cd2
Eduard Metzler <eduard.metzler@mail.ru>
2021-03-28 23:38:05 +0200

    2

--- my-project/backend/database.js
+++ my-project/backend/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const connection = "mongodb+srv://username:<password>@<cluster>/<database>?retryWrites=true&w=majority";
+const connection = "mongodb+srv://eduard:eduard@cluster0.o9l6j.azure.mongodb.net/<dbname>?retryWrites=true&w=majority";
 mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
     .then(() => console.log("Database Connected Successfully"))
     .catch(err => console.log(err));
